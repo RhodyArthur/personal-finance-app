fd41873641403f8514c84a14e87c3f313fc69258
RhodyArthur <rhodaarthur48@gmail.com>
2025-03-09 13:05:11 +0000

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'overview',
+    loadComponent: () => import('./features/overview/overview.component').then(m => m.OverviewComponent)
+    },
+    {
+        path: 'transactions',
+        loadComponent: () => import('./features/transactions/transactions.component').then(m => m.TransactionsComponent)
+    },
+    {
+        path: 'budegts',
+        loadComponent: () => import('./features/budgets/budgets.component').then(m => m.BudgetsComponent)
+    },
+    {
+        path: 'pots',
+        loadComponent: () => import('./features/pots/pots.component').then(m => m.PotsComponent)
+    },
+    {
+        path: 'recurring bills',
+        loadComponent: () => import('./features/recurring-bills/recurring-bills.component').then(m => m.RecurringBillsComponent)
+    },
+];
