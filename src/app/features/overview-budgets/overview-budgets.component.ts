3207503fb80b8341a823c541a984bd1f5d003fa6
RhodyArthur <rhodaarthur48@gmail.com>
2025-03-10 08:42:34 +0000

    complete overview  transactions

---
+++ src/app/features/overview-budgets/overview-budgets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-budgets',
+  imports: [],
+  templateUrl: './overview-budgets.component.html',
+  styleUrl: './overview-budgets.component.sass'
+})
+export class OverviewBudgetsComponent {
+
+}
