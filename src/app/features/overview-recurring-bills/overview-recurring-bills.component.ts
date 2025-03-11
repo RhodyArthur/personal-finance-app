aa607f4b71acab25f7091c210cd4a539ad2c6d4b
RhodyArthur <rhodaarthur48@gmail.com>
2025-03-11 11:21:38 +0000

    feat: add recurring bill summary template

--- src/app/features/overview-recurring-bills/overview-recurring-bills.component.ts
+++ src/app/features/overview-recurring-bills/overview-recurring-bills.component.ts
@@ -1,11 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Transaction } from '../../core/models/transactions';
 
 @Component({
   selector: 'app-overview-recurring-bills',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './overview-recurring-bills.component.html',
   styleUrl: './overview-recurring-bills.component.sass'
 })
 export class OverviewRecurringBillsComponent {
-  // recurringBills = input.required<[]>();
+  transactions = input.required<Transaction[]>();
+
+  recurringBills = computed(() => this.transactions().filter((transaction) => transaction.recurring === true));
+
+  paidBills = computed(() => this.recurringBills().filter((bill) => bill.amount > 0).reduce((acc, bill) => acc + bill.amount, 0))
 }
